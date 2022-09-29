61bc262ff46bdfaf4e9ecbd63057e68a1416d965
Mariana.Andrusiak <marianka518@gmail.com>
2022-09-29 17:10:44 +0300

    Multiplication table of 4

---
+++ src/com/operators_loops/MultiplicationTable.java
@@ -0,0 +1,11 @@
+package com.operators_loops;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%5d", ((i+1)*4));
+
+        }
+        System.out.println();
+    }
+}
