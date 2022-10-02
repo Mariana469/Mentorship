19eccaf563f60d3d86cf90d6b9f9d6b9b1374a02
Mariana.Andrusiak <marianka518@gmail.com>
2022-10-02 21:53:54 +0300

    Committed task DoubleTheNumber to a new branch Task

--- src/com/operators_loops/DoubleTheNumber.java
+++ src/com/operators_loops/DoubleTheNumber.java
@@ -1,2 +1,12 @@
-package com.operators_loops;public class DoubleTheNumber {
+package com.operators_loops;
+
+public class DoubleTheNumber {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 512) {
+            System.out.println(i);
+            i*=2;
+        }
+    }
 }
