911adfc59b97c403514308352c222ba5e9e6118d
Mariana.Andrusiak <marianka518@gmail.com>
2022-10-03 11:14:17 +0300

    Added constants to program for counting customer's deposit with FOR cycle

--- src/com/operators_loops/CountCustomersDeposit.java
+++ src/com/operators_loops/CountCustomersDeposit.java
@@ -4,11 +4,17 @@ import java.util.Scanner;
 import java.math.*;
 
 public class CountCustomersDeposit {
+
+    private static final String ЧЕРЕЗ = "Через ";
+    private static final String МІСЯЦІВ_ВАШ_ВКЛАД_БУДЕ = " місяців ваш вклад буде: ";
+    private static final String ВВЕДІТЬ_КІЛЬКІСТЬ_МІСЯЦІВ = "Введіть кількість місяців: ";
+    private static final String ВВЕДІТЬ_СУМУ_ВАШОГО_ВКЛАДУ = "Введіть суму вашого вкладу: ";
+
     public static void main(String[] args) {
-        System.out.println("Введіть суму вашого вкладу: ");
+        System.out.println(ВВЕДІТЬ_СУМУ_ВАШОГО_ВКЛАДУ);
         Scanner scanner = new Scanner(System.in);
         double sum = scanner.nextInt();
-        System.out.println("Введіть кількість місяців: ");
+        System.out.println(ВВЕДІТЬ_КІЛЬКІСТЬ_МІСЯЦІВ);
         Scanner scanner1 = new Scanner(System.in);
         int numberOfMonths = scanner1.nextInt();
         for (int i = 1; i<numberOfMonths;i++){
@@ -17,7 +23,7 @@ public class CountCustomersDeposit {
         BigDecimal bigDecimalSum = BigDecimal.valueOf(sum);
         BigDecimal bigDecimalSumRounded;
         bigDecimalSumRounded = bigDecimalSum.setScale(2,RoundingMode.HALF_DOWN);
-        System.out.println("Через " + numberOfMonths + " місяців ваш вклад буде: " + bigDecimalSumRounded);
+        System.out.println(ЧЕРЕЗ + numberOfMonths + МІСЯЦІВ_ВАШ_ВКЛАД_БУДЕ + bigDecimalSumRounded);
 
     }
 }
