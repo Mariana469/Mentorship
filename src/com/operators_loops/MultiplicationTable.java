2a1ef58b745eefc6bc3499cf1df0f8b1cac50470
Mariana469 <75381906+Mariana469@users.noreply.github.com>
2022-10-03 12:15:20 +0300

    Revert "Task for counting customer's deposit"

--- src/com/operators_loops/MultiplicationTable.java
+++ src/com/operators_loops/MultiplicationTable.java
@@ -3,8 +3,7 @@ package com.operators_loops;
 public class MultiplicationTable {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            int r = (4*(i+1));
-            System.out.println("4 * " + (i+1) + " = " + r);
+            System.out.printf("%5d", ((i+1)*4));
 
         }
         System.out.println();
