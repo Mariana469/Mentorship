a21064892b0c26c52a73ef188caa76182ff31756
Mariana.Andrusiak <marianka518@gmail.com>
2022-10-21 13:09:30 +0300

    Task 1 Rectangle

--- src/main/java/com/oop/Rectangle.java
+++ src/main/java/com/oop/Rectangle.java
@@ -1,4 +1,19 @@
 package com.oop;
 
 public class Rectangle {
+    private static int width = 9;
+    private static int length = 2;
+
+    public static void calculateSquare(){
+        System.out.println(width * length);
+    }
+    public static void calculatePerimeter(){
+        System.out.println((width + length) * 2);
+
+    }
+
+    public static void main(String[] args) {
+        Rectangle.calculateSquare();
+        Rectangle.calculatePerimeter();
+    }
 }
