70e395ab4fe2c6474d75de946a984f7cf2e42893
Mariana.Andrusiak <marianka518@gmail.com>
2022-10-21 12:03:48 +0300

    Committing suggestions after code review

--- src/main/java/com/arrays/FindArrayValue.java
+++ src/main/java/com/arrays/FindArrayValue.java
@@ -10,26 +10,13 @@ public class FindArrayValue {
 
     public static void main(String[] args) {
         int[] array = new int[]{30, 50, 62, 42, 72};
-
         System.out.println(THE_INPUT_ARRAY + Arrays.toString(array));
         int key = 30;
         System.out.println(KEY_TO_BE_SEARCHED_IS + key);
-        int first = 0;
-        int last=array.length-1;
-        int mid = (first + last)/2;
-        while( first <= last ){
-            if ( array[mid] < key ){
-                first = mid + 1;
-            }else if ( array[mid] == key ){
-                System.out.println(ELEMENT_IS_FOUND_AT_INDEX + mid);
-                break;
-            }else{
-                last = mid - 1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == key) {
+                System.out.println(ELEMENT_IS_FOUND_AT_INDEX + i);
             }
-            mid = (first + last)/2;
-        }
-        if ( first > last ){
-            System.out.println(ELEMENT_IS_NOT_FOUND);
         }
     }
 }
--- src/main/java/com/arrays/MaxMinValue.java
+++ src/main/java/com/arrays/MaxMinValue.java
@@ -10,7 +10,8 @@ public class MaxMinValue {
         int max = arr[0];
         int min = arr[0];
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) max = arr[i];
+            if (arr[i] > max)
+                max = arr[i];
         }
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] < min) min = arr[i];
