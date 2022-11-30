14a475f170870c059049b4acde02167cfbe56a2f
Mariana.Andrusiak <marianka518@gmail.com>
2022-11-30 17:15:18 +0200

    Finished all the tasks for 10th lesson

--- src/main/java/com/wrappers/GenericModule.java
+++ src/main/java/com/wrappers/GenericModule.java
@@ -1,15 +1,17 @@
 package com.wrappers;
 
-import java.util.Collection;
-
 public class GenericModule {
-    static class Professions<T> {
+    public interface Convertible<K, V> {
+        public V convertNumber(K k);
+    }
+
+    public static class Professions<T> {
+        StringBuilder sb = new StringBuilder("This is: ");
         private T numberOfSpecialists;
+
         public Professions(T numberOfSpecialists) {
             this.numberOfSpecialists = numberOfSpecialists;
         }
-
-        StringBuilder sb = new StringBuilder("This is: ");
 
         //Just wanted to try with StringBuilder for practice
         @Override
@@ -18,15 +20,32 @@ public class GenericModule {
         }
 
     }
-    static class DifferentNumbers<N extends Number> {
+
+    public static class DifferentNumbers<N extends Number> {
         private N maxNumber;
         private N minNumber;
         private N factNumber;
+    }
 
-        public void setValueOfNumber(N maxNumber, N minNumber, N factNumber){
-            this.maxNumber = maxNumber;
-            this.minNumber = minNumber;
-            this.factNumber = factNumber;
+    public static class MaxValueHolder<T extends Comparable<T>> {
+        private T maxValue;
+        private T originalValue;
+
+        public MaxValueHolder(T maxValue, T originalValue) {
+            this.maxValue = maxValue;
+            this.originalValue = originalValue;
+        }
+
+        public T getMaxValue() {
+            return maxValue;
+        }
+
+        public void put(T t) {
+            if (this.maxValue.compareTo(t) >= 0) {
+                originalValue = t;
+            } else {
+                throw new NullPointerException();
+            }
         }
     }
 }
