5e20c81c39a73d4a4c36e6d633ac59641dc90639
Mariana.Andrusiak <marianka518@gmail.com>
2022-12-05 15:15:23 +0200

    First task of eleventh lesson

---
+++ src/main/java/com/exception/PurchaseOrder.java
@@ -0,0 +1,16 @@
+package com.exception;
+
+public class PurchaseOrder {
+    public static void main(String[] args) {
+        try {
+            int poNumbers[] = new int[10];
+            poNumbers[11] = 123456;
+        } catch (ArrayIndexOutOfBoundsException outOfBoundsPoNumber) {
+            System.out.println("Array Index is Out Of Bounds");
+        }
+        finally {
+            System.out.println("Only 10 POs can be created");
+        }
+
+    }
+}
