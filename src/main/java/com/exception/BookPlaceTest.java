3bd04629d466f0e5226a859a1a47530edac39aeb
Mariana.Andrusiak <marianka518@gmail.com>
2022-12-06 16:15:09 +0200

    Fourth task of eleventh lesson

--- src/main/java/com/exception/BookPlaceTest.java
+++ src/main/java/com/exception/BookPlaceTest.java
@@ -3,8 +3,11 @@ package com.exception;
 public class BookPlaceTest {
     public static void main(String[] args) {
         BookPlaceManager manager = new BookPlaceManager();
+        Place anotherPlace = new Place();
         try {
             Place place = manager.book(21);
+            anotherPlace = null;
+            System.out.println(anotherPlace.toString());
         } catch (IncorrectPlaceNumberException e) {
             System.err.println(e);
         }
